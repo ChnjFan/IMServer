55ce696656b8339fc6838d69c5b938853de09292
Your Name <you@example.com>
2024-11-11 22:46:04 +0800

    新增route_server模块以及poco库安装脚本

---
+++ doc/README.md
@@ -0,0 +1,5 @@
+# 安装依赖
+
+执行 `third_party` 下的 `.sh` 文件安装依赖。
+
+- poco-1.13.3-release
---
+++ server/route_server/route_server.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file route_server.cpp
+ * @brief 路由服务器
+ */
+
+#include "Poco/Net/Net.h"
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
