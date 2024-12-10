ad9d0b9529dd34145154b77482c026daec3270ff
ChnjFan <chnj_lf@163.com>
2024-12-10 22:11:28 +0800

    新增文档

--- server/test/client_conn.cpp
+++ server/test/client_conn.cpp
@@ -3,54 +3,7 @@
  * @brief 客户端连接测试
  */
 
-#include <iostream>
-#include <string>
-#include "Poco/Net/StreamSocket.h"
-#include "Poco/Net/SocketAddress.h"
-#include "Poco/Util/IniFileConfiguration.h"
-#include "Poco/AutoPtr.h"
-#include "IMPdu.h"
-#include "ByteStream.h"
-
-#define BUFFER_SIZE 1024
-using Poco::Net::SocketAddress;
-using Poco::Net::StreamSocket;
-using Poco::AutoPtr;
-using Poco::Util::IniFileConfiguration;
-
 int main (int argc, const char * argv[])
 {
-    AutoPtr<IniFileConfiguration> pConfig(new IniFileConfiguration("conn_server_config.ini"));
-
-    std::string serverIP = "127.0.0.1";
-    int serverPort = pConfig->getInt("server.listen_port");
-
-
-
-    SocketAddress address(serverIP, serverPort);
-    StreamSocket socket(address);
-    char buffer[BUFFER_SIZE] = {0};
-    int sendlen = socket.sendBytes(buffer, 12);
-    std::cout << "send size: " << sendlen << std::endl;
-
-    Common::ByteStream data(1024);
-    // 循环接收数据
-    while (true)
-    {
-        // 检查是否有数据可读
-        if (socket.available())
-        {
-            // 接收数据
-            int len = socket.receiveBytes(buffer, BUFFER_SIZE);
-            if (len <= 0)
-                break;
-
-            data.write(buffer, len);
-            // 输出接收到的数据
-            std::cout << "recv size: " << data.peekUint32() << std::endl;
-        }
-    }
-
-    socket.close();
     return 0;
 }
