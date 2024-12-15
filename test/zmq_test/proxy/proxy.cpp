da7c3c0024f7e46fd54adedbddca90662112e283
ChnjFan <chnj_lf@163.com>
2024-12-15 22:48:06 +0800

    内部消息队列通信框架-测试第三方代码

--- test/zmq_test/proxy/proxy.cpp
+++ test/zmq_test/proxy/proxy.cpp
@@ -1,86 +1,102 @@
-//  Last value cache
-//  Uses XPUB subscription messages to re-send data
-
-#include <unordered_map>
+#include <future>
 #include <iostream>
+#include <string>
+#include <thread>
+
 #include "zmq.hpp"
+#include "zmq_addon.hpp"
 
-int main ()
-{
-    zmq::context_t context(1);
-    zmq::socket_t  frontend(context, ZMQ_SUB);
-    zmq::socket_t  backend(context, ZMQ_XPUB);
+void PublisherThread(zmq::context_t *ctx) {
+    //  Prepare publisher
+    zmq::socket_t publisher(*ctx, zmq::socket_type::pub);
+    publisher.bind("inproc://#1");
 
-    frontend.connect("tcp://localhost:5557");
-    backend.bind("tcp://*:5558");
+    // Give the subscribers a chance to connect, so they don't lose any messages
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
 
-    //  Subscribe to every single topic from publisher
-    frontend.set(zmq::sockopt::subscribe, "");
-
-    //  Store last instance of each topic in a cache
-    std::unordered_map<std::string, std::string> cache_map;
-
-    zmq::pollitem_t items[2] = {
-            { static_cast<void*>(frontend), 0, ZMQ_POLLIN, 0 },
-            { static_cast<void*>(backend), 0, ZMQ_POLLIN, 0 }
-    };
-
-    //  .split main poll loop
-    //  We route topic updates from frontend to backend, and we handle
-    //  subscriptions by sending whatever we cached, if anything:
-    while (1)
-    {
-        if (zmq::poll(items, 2, 1000) == -1)
-            break; //  Interrupted
-
-        //  Any new topic data we cache and then forward
-        if (items[0].revents & ZMQ_POLLIN)
-        {
-            zmq::message_t message;
-            frontend.recv(message);
-            std::string topic = std::string((char*)(message.data()), message.size());
-            frontend.recv(message);
-            std::string data = std::string((char*)(message.data()), message.size());
-
-            std::cout << topic << data << std::endl;
-
-            if (topic.empty())
-                break;
-
-            cache_map[topic] = data;
-
-            message.rebuild(topic);
-            backend.send(message, zmq::send_flags::sndmore);
-            message.rebuild(data);
-            backend.send(message, zmq::send_flags::none);
-        }
-
-        //  .split handle subscriptions
-        //  When we get a new subscription, we pull data from the cache:
-        if (items[1].revents & ZMQ_POLLIN) {
-            zmq::message_t msg;
-
-            backend.recv(&msg);
-            if (msg.size() == 0)
-                break;
-
-            //  Event is one byte 0=unsub or 1=sub, followed by topic
-            uint8_t *event = (uint8_t *)msg.data();
-            if (event[0] == 1) {
-                std::string topic((char *)(event+1), msg.size()-1);
-
-                auto i = cache_map.find(topic);
-                if (i != cache_map.end())
-                {
-                    zmq::message_t message;
-                    message.rebuild(topic);
-                    backend.send(message, zmq::send_flags::sndmore);
-                    message.rebuild(i->second);
-                    backend.send(message, zmq::send_flags::none);
-                }
-            }
-        }
+    while (true) {
+        //  Write three messages, each with an envelope and content
+        publisher.send(zmq::str_buffer("A"), zmq::send_flags::sndmore);
+        publisher.send(zmq::str_buffer("Message in A envelope"));
+        publisher.send(zmq::str_buffer("B"), zmq::send_flags::sndmore);
+        publisher.send(zmq::str_buffer("Message in B envelope"));
+        publisher.send(zmq::str_buffer("C"), zmq::send_flags::sndmore);
+        publisher.send(zmq::str_buffer("Message in C envelope"));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+}
 
-    return 0;
+void SubscriberThread1(zmq::context_t *ctx) {
+    //  Prepare subscriber
+    zmq::socket_t subscriber(*ctx, zmq::socket_type::sub);
+    subscriber.connect("inproc://#1");
+
+    //  Thread2 opens "A" and "B" envelopes
+    subscriber.set(zmq::sockopt::subscribe, "A");
+    subscriber.set(zmq::sockopt::subscribe, "B");
+
+    while (1) {
+        // Receive all parts of the message
+        std::vector<zmq::message_t> recv_msgs;
+        zmq::recv_result_t result =
+                zmq::recv_multipart(subscriber, std::back_inserter(recv_msgs));
+        assert(result && "recv failed");
+        assert(*result == 2);
+
+        std::cout << "Thread2: [" << recv_msgs[0].to_string() << "] "
+                  << recv_msgs[1].to_string() << std::endl;
+    }
+}
+
+void SubscriberThread2(zmq::context_t *ctx) {
+    //  Prepare our context and subscriber
+    zmq::socket_t subscriber(*ctx, zmq::socket_type::sub);
+    subscriber.connect("inproc://#1");
+
+    //  Thread3 opens ALL envelopes
+    subscriber.set(zmq::sockopt::subscribe, "");
+
+    while (1) {
+        // Receive all parts of the message
+        std::vector<zmq::message_t> recv_msgs;
+        zmq::recv_result_t result =
+                zmq::recv_multipart(subscriber, std::back_inserter(recv_msgs));
+        assert(result && "recv failed");
+        assert(*result == 2);
+
+        std::cout << "Thread3: [" << recv_msgs[0].to_string() << "] "
+                  << recv_msgs[1].to_string() << std::endl;
+    }
+}
+
+int main() {
+    /*
+     * No I/O threads are involved in passing messages using the inproc transport.
+     * Therefore, if you are using a ØMQ context for in-process messaging only you
+     * can initialise the context with zero I/O threads.
+     *
+     * Source: http://api.zeromq.org/4-3:zmq-inproc
+     */
+    zmq::context_t ctx(0);
+
+    auto thread1 = std::async(std::launch::async, PublisherThread, &ctx);
+
+    // Give the publisher a chance to bind, since inproc requires it
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+    auto thread2 = std::async(std::launch::async, SubscriberThread1, &ctx);
+    auto thread3 = std::async(std::launch::async, SubscriberThread2, &ctx);
+    thread1.wait();
+    thread2.wait();
+    thread3.wait();
+
+    /*
+     * Output:
+     *   An infinite loop of a mix of:
+     *     Thread2: [A] Message in A envelope
+     *     Thread2: [B] Message in B envelope
+     *     Thread3: [A] Message in A envelope
+     *     Thread3: [B] Message in B envelope
+     *     Thread3: [C] Message in C envelope
+     */
 }
--- test/zmq_test/server/server.cpp
+++ test/zmq_test/server/server.cpp
@@ -2,49 +2,104 @@
 //  Sends out 1,000 topics and then one random update per second
 
 #include <thread>
-#include <chrono>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <csignal>
 #include "zmq.hpp"
+#include <iostream>
+
+class server_worker {
+public:
+    server_worker(zmq::context_t &ctx, int sock_type)
+            : ctx_(ctx),
+              worker_(ctx_, sock_type)
+    {}
+
+    void work() {
+        worker_.connect("inproc://backend");
+
+        try {
+            while (true) {
+                zmq::message_t identity;
+                zmq::message_t msg;
+                zmq::message_t copied_id;
+                zmq::message_t copied_msg;
+                worker_.recv(&identity);
+                worker_.recv(&msg);
+
+                std::cout << identity.to_string() << msg.to_string() << std::endl;
+
+                for (int reply = 0; reply < 5; ++reply) {
+                    sleep(1000);
+                    copied_id.copy(&identity);
+                    copied_msg.copy(&msg);
+                    worker_.send(copied_id, ZMQ_SNDMORE);
+                    worker_.send(copied_msg);
+                }
+            }
+        }
+        catch (std::exception &e) {}
+    }
+
+
+
+private:
+    zmq::context_t &ctx_;
+    zmq::socket_t worker_;
+};
+
+class server_pub {
+public:
+    server_pub(zmq::context_t &ctx, int sock_type) : publisher(ctx, sock_type) { }
+
+    void work() {
+        publisher.bind("tcp://*:5571");
+
+        while (1) {
+            std::string str("publish service");
+            zmq::message_t msg(str);
+            publisher.send(msg, zmq::send_flags::none);
+            sleep(3);
+        }
+    }
+private:
+    zmq::socket_t publisher;
+};
 
 int main (int argc, char *argv [])
 {
-    zmq::context_t context(1);
-    zmq::socket_t publisher(context, ZMQ_PUB);
+    zmq::context_t ctx_(1);
+    zmq::socket_t frontend_(ctx_, ZMQ_ROUTER);
+    zmq::socket_t backend_(ctx_, ZMQ_DEALER);
 
-    //  Initialize random number generator
-    srandom ((unsigned) time (NULL));
+    frontend_.bind("tcp://*:5570");
+    backend_.bind("inproc://backend");
 
-    if (argc == 2)
-        publisher.bind(argv [1]);
-    else
-        publisher.bind("tcp://*:5557");
+    std::vector<server_worker *> worker;
+    std::vector<std::thread *> worker_thread;
+    for (int i = 0; i < 5; ++i) {
+        worker.push_back(new server_worker(ctx_, ZMQ_DEALER));
 
-    //  Ensure subscriber connection has time to complete
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
-    //  Send out all 1,000 topic messages
-//    int topic_nbr;
-//    for (topic_nbr = 0; topic_nbr < 1000; topic_nbr++) {
-//        std::stringstream ss;
-//        ss << std::dec << std::setw(3) << std::setfill('0') << topic_nbr;
-//        zmq::message_t message(ss.str());
-//        publisher.send(message, zmq::send_flags::sndmore);
-//        message.rebuild(std::string("Save Roger"));
-//        publisher.send(message, zmq::send_flags::none);
-//    }
-
-    //  Send one random update per second
-
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::stringstream ss;
-        ss << std::dec << std::setw(3) << std::setfill('0');
-
-        zmq::message_t message(ss.str());
-        message.rebuild(std::string("Save Roger"));
-        publisher.send(message, zmq::send_flags::sndmore);
-        message.rebuild(std::string("Off with his head!"));
-        publisher.send(message, zmq::send_flags::none);
-    while (1) {
-
+        worker_thread.push_back(new std::thread(std::bind(&server_worker::work, worker[i])));
+        worker_thread[i]->detach();
     }
+
+    server_pub publish(ctx_, ZMQ_PUB);
+    std::thread *pub = new std::thread(std::bind(&server_pub::work, &publish));
+    pub->detach();
+
+    try {
+        // 使用句柄和from_handle标签构造一个socket_ref
+        zmq::proxy(zmq::socket_ref(zmq::from_handle, frontend_.handle()),
+                   zmq::socket_ref(zmq::from_handle, backend_.handle()));
+    }
+    catch (std::exception &e) {}
+
+    for (int i = 0; i < 5; ++i) {
+        delete worker[i];
+        delete worker_thread[i];
+    }
+
     return 0;
 }
