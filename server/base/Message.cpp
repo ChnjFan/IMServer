a0576e879057bb76e4c11c2c0cf23e51bdea26d5
ChnjFan <chnj_lf@163.com>
2024-12-30 23:20:52 +0800

    重构服务端网络框架

--- server/base/Message.cpp
+++ server/base/Message.cpp
@@ -11,44 +11,68 @@
 
 Base::Message::Message(): length(0), typeLen(0), typeName(""), checkSum(0), body(DEFAULT_BODY_LEN) { }
 
-Base::Message::Message(Base::ByteStream &body, std::string &typeName)
-                    : length(2*sizeof(uint32_t) + typeName.length() + 1 + body.size())
+Base::Message::Message(ByteBuffer &body, std::string typeName)
+                    : length(DEFAULT_HEADER_LEN + typeName.length() + 1 + body.size())
                     , typeLen(typeName.length() + 1)
                     , typeName(typeName)
                     , checkSum(0)
                     , body(body) {
     Poco::Checksum checker(Poco::Checksum::TYPE_ADLER32);
-    checker.update((const char*)body.data(), body.size());
+    checker.update((const char*)body.begin(), body.size());
     checkSum = checker.checksum();
 }
 
-Base::MessagePtr Base::Message::getMessage(Base::ByteStream &data) {
-    uint32_t len = data.peekUint32();
-
-    // 数据不完整，继续等待接收数据。其他情况消息内容有问题，需要跳过消息。
-    if (len > static_cast<uint32_t>(data.size()) || 0 == len)
-        return nullptr;
-
+Base::MessagePtr Base::Message::getMessage(ByteBuffer &data) {
     Base::MessagePtr pMessage = std::make_shared<Message>();
+    uint32_t readCount = 0;
+    unsigned char *pBuffer = data.begin();
 
     // 读取消息头
-    pMessage->length = data.readUint32();
-    pMessage->typeLen = data.readUint32();
+    std::copy(pBuffer, pBuffer + sizeof(pMessage->length), &pMessage->length);
+    pBuffer += sizeof(pMessage->length);
+    readCount += sizeof(pMessage->length);
+    pMessage->length = Poco::ByteOrder::fromNetwork(pMessage->length);
+    // 缓存内容不能组成一个消息长度
+    if (pMessage->length < data.sizeBytes())
+        return nullptr;
+    // 消息不能超过最大消息长度MAX_MESSAGE_LEN，超长属于异常，需要关闭连接让客户端重连
+    if (pMessage->length > MAX_MESSAGE_LEN)
+        throw Base::Exception("Message length error, len = " + pMessage->length);
 
-    pMessage->typeName = data.readString(pMessage->typeLen);
+    // 类型长度
+    std::copy(pBuffer, pBuffer + sizeof(pMessage->typeLen), &pMessage->typeLen);
+    pBuffer += sizeof(pMessage->typeLen);
+    readCount += sizeof(pMessage->typeLen);
+    pMessage->typeLen = Poco::ByteOrder::fromNetwork(pMessage->typeLen);
+    if (pBuffer[pMessage->typeLen] != '\0' || pMessage->typeLen == 0)
+        throw Base::Exception("Message typeName error, typeLen = " + std::to_string(pMessage->typeLen) + " typeName = " + (char*)pBuffer);
 
-    Base::ByteStream temp = data.read(pMessage->length - pMessage->typeLen - 1 - 3*sizeof(uint32_t));
-    pMessage->body = temp;
+    // 类型名称
+    pMessage->typeName = (char*)pBuffer;
+    pBuffer += pMessage->typeLen + 1;
+    readCount += pMessage->typeLen + 1;
 
-    pMessage->checkSum = data.readUint32();
+    // 消息体
+    int bodySize = pMessage->length - readCount - sizeof(pMessage->checkSum);
+    pMessage->body.resize(bodySize);
+    std::copy(pBuffer, pBuffer + bodySize, pMessage->body.begin());
+    pBuffer += bodySize;
+    readCount += bodySize;
+
     // 计算校验和，校验失败返回空，此时该消息也从缓冲区中读完
+    std::copy(pBuffer, pBuffer + sizeof(pMessage->checkSum), &pMessage->checkSum);
+    pMessage->checkSum = Poco::ByteOrder::fromNetwork(pMessage->checkSum);
     Poco::Checksum checker(Poco::Checksum::TYPE_ADLER32);
-    checker.update((const char*)pMessage->body.data(), pMessage->body.size());
+    checker.update((const char*)pMessage->body.begin(), pMessage->body.size());
     if (pMessage->checkSum != checker.checksum())
         throw Exception("Message check sum error");
 
-    if (pMessage->typeName.length() == 0)
-        throw Exception("Message type name error");
+    if (readCount < data.size())
+    {
+        ByteBuffer tempBuffer(data.size() - readCount);
+        std::copy(data.begin() + readCount, data.end(), tempBuffer.begin());
+        data.swap(tempBuffer);
+    }
 
     return pMessage;
 }
@@ -69,20 +93,20 @@ Base::ProtobufMsgPtr Base::Message::deserialize() {
     return pMessage;
 }
 
-uint32_t Base::Message::serialize(char *buf, uint32_t bufSize) {
+uint32_t Base::Message::serialize(unsigned char *buf, uint32_t bufSize) {
     if (length + sizeof(uint32_t) < bufSize)//缓存长度不够
         return 0;
 
     typeLen = typeName.length()+1;
     memcpy(buf, &length, sizeof(uint32_t));
     memcpy(buf + sizeof(uint32_t), &typeLen, sizeof(uint32_t));
-    strncat(buf, typeName.c_str(), bufSize);
+    strncat((char*)buf, typeName.c_str(), bufSize);
 
     // 计算消息头长度
     uint32_t len = 2 * sizeof(uint32_t) + typeLen;
 
     if (len + body.size() <= bufSize) {
-        memcpy(buf+len, body.data(), body.size());
+        memcpy(buf+len, body.begin(), body.size());
         len += body.size();
     }
     else {
@@ -91,7 +115,7 @@ uint32_t Base::Message::serialize(char *buf, uint32_t bufSize) {
 
     //添加消息校验和
     Poco::Checksum checker(Poco::Checksum::TYPE_ADLER32);
-    checker.update((const char*)body.data(), body.size());
+    checker.update((const char*)body.begin(), body.size());
     checkSum = checker.checksum();
     memcpy(buf+len, &checkSum, sizeof(uint32_t));
     len += sizeof(uint32_t);
--- server/base/Message.h
+++ server/base/Message.h
@@ -7,14 +7,15 @@
 
 #include <memory>
 #include <string>
-#include "ByteStream.h"
 #include "google/protobuf/message.h"
+#include "Poco/Buffer.h"
 
 namespace Base {
 
-    class Message;
-    typedef std::shared_ptr<Message> MessagePtr;
-    typedef std::shared_ptr<google::protobuf::Message> ProtobufMsgPtr;
+class Message;
+using MessagePtr = std::shared_ptr<Message>;
+using ProtobufMsgPtr = std::shared_ptr<google::protobuf::Message>;
+using ByteBuffer = Poco::Buffer<unsigned char>;
 
 /**
  * @class Message
@@ -24,7 +25,7 @@ namespace Base {
 class Message {
 public:
     Message();
-    Message(Base::ByteStream& body, std::string& typeName);
+    Message(ByteBuffer& body, std::string typeName);
 
     /**
      * @brief 消息解码
@@ -32,7 +33,7 @@ public:
      * @return 请求消息
      * @throws Exception 消息解码异常
      */
-    static MessagePtr getMessage(Base::ByteStream &data);
+    static MessagePtr getMessage(ByteBuffer &data);
 
     /**
      * @brief 消息反序列化
@@ -46,13 +47,14 @@ public:
      * @param bufSize 缓冲区长度
      * @return 序列化消息长度
      */
-    uint32_t serialize(char *buf, uint32_t bufSize);
+    uint32_t serialize(unsigned char *buf, uint32_t bufSize);
 
     uint32_t size();
 
     std::string& getTypeName();
 
 private:
+    static constexpr uint32_t MAX_MESSAGE_LEN = 5 * 1024;
     static constexpr uint32_t DEFAULT_BODY_LEN = 1024;
     static constexpr uint32_t DEFAULT_HEADER_LEN = 3*sizeof(uint32_t);
 
@@ -75,7 +77,7 @@ private:
     /**
      * @brief 消息体
      */
-    Base::ByteStream body;
+    ByteBuffer body;
 };
 
 }
